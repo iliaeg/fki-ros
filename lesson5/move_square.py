41e1ea0f3a5e33c3ad32a56a9a36f1569ba41633
iliaeg <egorovilyak@yandex.ru>
2019-10-24 12:17:24 +0300

    move_square (not finished)

--- lesson5/move_square.py
+++ lesson5/move_square.py
@@ -16,15 +16,64 @@ def quaternion_to_theta(orientation):
 
     return math.degrees(math.atan2(t1, t2))
 
-angle = 0
-Point position = 0
+vel_msg = Twist()
+vel_msg.linear.x = 0
+vel_msg.linear.y = 0
+vel_msg.linear.z = 0
+vel_msg.angular.x = 0
+vel_msg.angular.y = 0
+vel_msg.angular.z = 0
 
-def get_angle(odom_data):
-    # rostopic echo /odom
-    global angle
-    angle =  quaternion_to_theta(odom_data.pose.pose.orientation)
-    global position
-    position = odom_data.pose.pose.position
+class RobotState:
+    def __init__(self, forward, start_x, start_y, turn, start_angle, is_init):
+        self.forward = forward
+        self.forward_counter = 0
+        self.start_x = start_x
+        self.start_y = start_y
+        self.turn = turn
+        self.turn_counter = 0
+        self.start_angle = start_angle
+        self.is_init = is_init
+
+    def init_state(self, start_x, start_y, start_angle):
+        self.start_x = start_x
+        self.start_y = start_y
+        self.start_angle = start_angle
+
+robot = RobotState(False, 0.0, 0.0, False, 0.0, False)
+
+class Pose:
+    def __init__(self, x, y, theta):
+        self.x = x
+        self.y = y
+        self.theta = theta
+
+def go_forward():
+    global robot
+    global vel_msg
+    vel_msg.linear.x = 0.1
+    robot.forward = True
+
+def stop_forward():
+    global robot
+    global vel_msg
+    vel_msg.linear.x = 0.0
+    robot.forward = False
+
+def go_turn():
+    global robot
+    global vel_msg
+    vel_msg.angular.z = 0.2
+    robot.turn = True
+
+def stop_turn():
+    global robot
+    global vel_msg
+    vel_msg.angular.z = 0.0
+    robot.turn = False
+
+def distance(x, y, x_start, y_start):
+    return math.sqrt( (x-x_start)**2 + (y-y_start)**2 )
 
 # # from tf.transformations import *
 
@@ -34,106 +83,73 @@ def get_angle(odom_data):
 # # # Преобразование из кватерниона в углы Эйлера
 # # euler = euler_from_quaternion(quaternion)
 
-# # Простая функция "каллбек". Данная функция вызываеться
-# # в момент когда появились данные. В нашем примере
-# # выводи данные на экран
-# def callback(msg):
-#     # rostopic echo /odom
-#     print "X = ",  msg.pose.pose.position.x
-#     print "Y = ",  msg.pose.pose.position.y
-#     print "theta = ", quaternion_to_theta(msg.pose.pose.orientation)
+# A subscriber to the topic '/turtle1/pose'. self.update_pose is called
+# when a message of type Pose is received.
+pose = Pose(0, 0, 0)
 
-# # Инициализируем ноду ROS
-# rospy.init_node('temp_odom_subscr_1')
-# # Создаем подписчиака, в параметрах указываем
-# # Название топика, тип сообщения, имя функцим каллбека
-# sub = rospy.Subscriber('odom', Odometry, callback)
-# # Выводим информационное сообщение
-# # print sub.header
+#distance to go forward in meters
+s = 0.2
 
-# rospy.loginfo("I am a odom_subscriber_1")
-# # # Передаем у
-# rospy.spin()
+def update_pose(data):
+    """Callback function which is called when a new message of type Pose is
+        received by the subscriber."""
+    # # rostopic echo /odom
+    # print "X = ",  msg.pose.pose.position.x
+    # print "Y = ",  msg.pose.pose.position.y
+    # print "theta = ", quaternion_to_theta(msg.pose.pose.orientation)
+    global robot, pose
+    pose.x = data.pose.pose.position.x
+    pose.y = data.pose.pose.position.y
+    pose.theta = quaternion_to_theta(data.pose.pose.orientation)
+    if not robot.is_init:
+        robot.init_state(pose.x, pose.y, pose.theta)
+        robot.is_init = True
+    global s
+    if robot.forward:
+        if distance(pose.x, pose.y, robot.start_x, robot.start_y) >= s:
+            stop_forward()
+            robot.forward_counter += 1
+            # robot.init_state(pose.x, pose.y, pose.theta)
+            robot.is_init = False
+            go_turn()
+    if robot.turn:
+        if abs(pose.theta - robot.start_angle) >= 90:
+            stop_turn()
+            robot.turn_counter += 1
+            if robot.turn_counter == 4:
+                exit(0)
+            # robot.init_state(pose.x, pose.y, pose.theta)
+            robot.is_init = False
+            go_forward()
 
-def get_angle_wrapper():
-    # make node
-    # rospy.init_node('ilia_odometry', anonymous=True)
-    sub = rospy.Subscriber('odom', Odometry, get_angle)
+    # print "X = ", pose.x #," ", round(pose.x, 4)
+    # print "robot X = ", robot.start_x
+    # print "Y = ", pose.y #," ", round(pose.y, 4)
+    # print "robot Y = ", robot.start_y
+    print "theta = ", pose.theta
+    print "robot theta = ", robot.start_angle
+
+pose_subscriber = rospy.Subscriber('/odom', Odometry, update_pose)
 
 def move():
+    print "Hello Im robot"
     # Starts a new node
     # rospy.init_node('robot_turtle_node_1', anonymous=True)
     velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-    vel_forward_msg = Twist()
-
-    print("Let's move your robot")
-    # Set variable to move forward with speed x
-    vel_forward_msg.linear.x = 0.1
-    vel_forward_msg.linear.y = 0
-    vel_forward_msg.linear.z = 0
-    vel_forward_msg.angular.x = 0
-    vel_forward_msg.angular.y = 0
-    vel_forward_msg.angular.z = 0
-
-    vel_turn_msg = Twist()
-    # Set variable to make U-turn
-    vel_turn_msg.linear.x = 0
-    vel_turn_msg.linear.y = 0
-    vel_turn_msg.linear.z = 0
-    vel_turn_msg.angular.x = 0
-    vel_turn_msg.angular.y = 0
-    vel_turn_msg.angular.z = 1.2
-
-    # while not rospy.is_shutdown():
-
-    #distance to go forward in meters
-    s = 0.2
-
-    for i in range(4):
-        ### Move s meters forward
-        # Setting the current time
-        t0 = rospy.Time.now().to_sec()
-        t1 = t0
-
-        while (t1-t0)*vel_forward_msg.linear.x < s:
-            #Publish the velocity
-            velocity_publisher.publish(vel_forward_msg)
-            #Takes actual time
-            t1 = rospy.Time.now().to_sec()
-
-        ### Make 90-degree turn
-        # Setting the current angle
-        get_angle_wrapper()
-        d0 = angle
-        d1 = d0
-
-        while (abs(d1-d0)) < 90.0:
-            #Publish the velocity
-            velocity_publisher.publish(vel_turn_msg)
-            #Takes actual angle
-            get_angle_wrapper()
-            d1 = angle
-            # print "d0 = ", d0
-            # print "d1 = ", d1
-            print "abs = ", abs(d1-d0)
-
-    #After the loop, stops the robot
-    vel_forward_msg.linear.x = 0
-    #Force the robot to stop
-    velocity_publisher.publish(vel_forward_msg)
-
-# def begin():
-#     while not rospy.is_shutdown():
-#         # make node
-#         rospy.init_node('ilia_odometry', anonymous=True)
-#         sub = rospy.Subscriber('odom', Odometry, move)
+    t0 = rospy.Time.now().to_sec()
+    go_forward()
+    while not rospy.is_shutdown():
+        velocity_publisher.publish(vel_msg)
+        t1 = rospy.Time.now().to_sec()
+        if (t1-t0 == 10):
+            rospy.signal_shutdown('Quit')
 
 if __name__ == '__main__':
-    try:
-        # begin()
-        # make node
-        rospy.init_node('ilia_odometry', anonymous=True)
-        # sub = rospy.Subscriber('odom', Odometry, move)
-        # #Testing our function
-        move()
-    except rospy.ROSInterruptException: pass
+    # try:
+    # begin()
+    # make node
+    rospy.init_node('ilia_odometry', anonymous=True)
+    # sub = rospy.Subscriber('odom', Odometry, move)
+    # #Testing our function
+    move()
+    # except rospy.ROSInterruptException: pass
